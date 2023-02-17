2e0d6e06bf44708f66f80e5acfb68e20f38683aa
Brian Wirsing <bwirsing@ford.com>
2023-02-16 22:52:44 -0500

    Extend input device classes

--- src/main/java/frc/robot/input/Joystick.java
+++ src/main/java/frc/robot/input/Joystick.java
@@ -4,27 +4,30 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-public class Joystick implements InputDevice {
+public class Joystick extends CommandJoystick implements InputDevice {
     private static final double DEADBAND = 0.15;
-    private final CommandJoystick joystick = new CommandJoystick(0);
+
+    public Joystick() {
+        super(0);
+    }
 
     @Override
     public double getHorizontalSpeed() {
-        return MathUtil.applyDeadband(-joystick.getX(), DEADBAND);
+        return MathUtil.applyDeadband(-getX(), DEADBAND);
     }
 
     @Override
     public double getVerticalSpeed() {
-        return MathUtil.applyDeadband(-joystick.getY(), DEADBAND);
+        return MathUtil.applyDeadband(-getY(), DEADBAND);
     }
 
     @Override
     public double getAngularSpeed() {
-        return MathUtil.applyDeadband(joystick.getTwist(), DEADBAND);
+        return MathUtil.applyDeadband(getTwist(), DEADBAND);
     }
 
     @Override
     public Trigger resetHeading() {
-        return joystick.button(7);
+        return button(7);
     }
 }
--- src/main/java/frc/robot/input/XboxController.java
+++ src/main/java/frc/robot/input/XboxController.java
@@ -4,27 +4,30 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-public class XboxController implements InputDevice {
+public class XboxController extends CommandXboxController implements InputDevice {
     private static final double DEADBAND = 0.15;
-    private final CommandXboxController xboxController = new CommandXboxController(0);
+
+    public XboxController() {
+        super(0);
+    }
 
     @Override
     public double getHorizontalSpeed() {
-        return MathUtil.applyDeadband(-xboxController.getLeftX(), DEADBAND);
+        return MathUtil.applyDeadband(-getLeftX(), DEADBAND);
     }
 
     @Override
     public double getVerticalSpeed() {
-        return MathUtil.applyDeadband(-xboxController.getLeftY(), DEADBAND);
+        return MathUtil.applyDeadband(-getLeftY(), DEADBAND);
     }
 
     @Override
     public double getAngularSpeed() {
-        return MathUtil.applyDeadband(xboxController.getRightX(), DEADBAND);
+        return MathUtil.applyDeadband(getRightX(), DEADBAND);
     }
 
     @Override
     public Trigger resetHeading() {
-        return xboxController.start();
+        return start();
     }
 }
