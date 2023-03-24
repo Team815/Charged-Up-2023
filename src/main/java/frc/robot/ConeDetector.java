ef2c9f4c8e4f8150efc4e8457a68fb0ecfe7f347
mkeysae2 <45372295+mkeysae2@users.noreply.github.com>
2023-03-23 20:12:08 -0400

    Add cone detector

---
+++ src/main/java/frc/robot/ConeDetector.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.SerialPort;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class ConeDetector {
+    private final SerialPort serialPort = new SerialPort(115200, SerialPort.Port.kMXP, 8, SerialPort.Parity.kNone);
+
+    public Optional<Integer> detect() {
+        var reading = serialPort.read(serialPort.getBytesReceived());
+        var readingString = new String(reading, StandardCharsets.UTF_8);
+
+        if (!readingString.endsWith("\n"))
+            return Optional.empty();
+
+        var readings =
+            Arrays
+                .stream(readingString.split("[\r\n]"))
+                .filter(s -> !s.isEmpty())
+                .toArray(String[]::new);
+
+        if(readings.length == 0)
+            return Optional.empty();
+
+        var value = Integer.valueOf(readings[readings.length - 1]);
+        return Optional.of(value);
+    }
+}
--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -3,14 +3,23 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.ConeDetector;
 
 public class Claw extends SubsystemBase {
     private final Compressor compressor;
     private final Solenoid solenoid;
+    private final ConeDetector coneDetector;
 
     public Claw(Compressor compressor, Solenoid solenoid) {
         this.compressor = compressor;
         this.solenoid = solenoid;
+        coneDetector = new ConeDetector();
+    }
+
+    @Override
+    public void periodic() {
+        var coneDectected = coneDetector.detect();
+        System.out.println(coneDectected.orElse(-1));
     }
 
     public void open() {
